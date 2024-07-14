69a1cb131dfb8ae04b004c82587d202b76bae303
DabAZ <dabaz@dabaz.me>
2024-07-15 06:06:04 +0800

    add the image for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className='text-4xl font-serif'>
-      昆仑巢
+    <div>
+      <Image
+        src="/images/hero.jpg"
+        alt="昆仑巢的照片。"
+        width={1920}
+        height={800}
+        sizes='(100vw) 100vw, (50vw) 50vw, (33vw) 33vw, (25vw) 25vw'
+        quality={75}
+        priority={true}
+        placeholder='blur'
+        blurDataURL='data:image/jpeg;base64,/9j/....'
+        style={{
+          width: '100%',
+          height: 'auto',
+          objectFit: 'cover',
+        }}
+      />
     </div>
   );
 }
