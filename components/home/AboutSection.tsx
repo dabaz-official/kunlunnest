774179b2db4f51923196b8e51adc6b80088d07e2
DabAZ <dabaz@dabaz.me>
2024-07-18 02:00:45 +0800

    Update AboutSection.tsx

--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Text from '@/components/ui/Text';
+import Text from '@/components/ui/text';
 
 export function AboutSection() {
   return (
