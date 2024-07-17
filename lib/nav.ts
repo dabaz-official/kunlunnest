80b53e02ad688e2a2ff4df0e529585ed46cb173a
DabAZ <dabaz@dabaz.me>
2024-07-17 17:20:11 +0800

    Update nav.ts

--- lib/nav.ts
+++ lib/nav.ts
@@ -1,39 +1,39 @@
-import { HiOutlineDatabase } from "react-icons/hi";
-import {
-  HiOutlineBackward,
-  HiOutlineBookmark,
-  HiOutlineBookOpen,
-  HiOutlineBriefcase,
-  HiOutlineBuildingLibrary,
-  HiOutlineCalendarDays, 
-  HiOutlineChatBubbleBottomCenterText, 
-  HiOutlineChatBubbleOvalLeftEllipsis, 
-  HiOutlineInboxArrowDown, 
-  HiOutlineInformationCircle,
-  HiOutlinePrinter,
-  HiOutlineUserGroup
-} from "react-icons/hi2";
+export const headerNavItems = [
+  {
+    name: '关于',
+    href: '/about',
+  },
+  {
+    name: '巢动态',
+    href: '/news',
+  },
+  {
+    name: '开放实验室',
+    href: '/labs',
+  },
+  {
+    name: '巢产品',
+    href: '/products',
+  },
+  {
+    name: '巢脉',
+    href: '/network',
+  },
+]
 
-export const navItems = {
-  "昆仑故事": [
-    { name: '关于我们', href: '/about', icon: HiOutlineInformationCircle },
-    { name: '活动预告', href: 'http://dayuoa.com/kunlunnest/eventplan', icon: HiOutlineCalendarDays },
-    { name: '活动回顾', href: 'http://dayuoa.com/kunlunnest/event', icon: HiOutlineBackward },
+export const footerNavItems = {
+  main: [
+    ...headerNavItems,
+    {
+      name: '联系我们',
+      href: '/contact',
+    },
+    {
+      name: '加入我们',
+      href: '/jobs',
+    },
   ],
-  "内容生态": [
-    { name: '苏菂说', href: '#', icon: HiOutlineChatBubbleBottomCenterText },
-    { name: '巢青年', href: '#', icon:HiOutlineUserGroup },
-    { name: 'CEO 聊日常', href: '#', icon: HiOutlineChatBubbleOvalLeftEllipsis },
-    { name: '3D 打印开放实验室', href: 'http://dayuoa.com/info/kopen-3dp', icon: HiOutlinePrinter },
-    { name: '科技历史博物馆', href: 'http://vjianjie.com/e-museum', icon: HiOutlineBuildingLibrary },
-  ],
-  "巢脉": [
-    { name: '资源库', href: 'http://dayuoa.com/hub/kunlunnest', icon: HiOutlineDatabase },
-    { name: '知识库', href: 'http://dayuoa.com/info/kunlunnest', icon: HiOutlineBookOpen },
-    { name: '申请入库', href: 'http://dayuoa.com/hub/apply/kunlunnest', icon: HiOutlineBookmark },
-  ],
-  "联系我们": [
-    { name: '招募信息', href: '#', icon: HiOutlineBriefcase },
-    { name: '场地申请', href: 'http://wzurl.cn/wj/9HDB8VeMh6', icon: HiOutlineInboxArrowDown },
-  ],
+  social: [
+
+  ]
 }
