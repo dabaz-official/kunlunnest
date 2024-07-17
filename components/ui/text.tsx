2dc8179dc61427636b71d609949b949fcf1bd045
DabAZ <dabaz@dabaz.me>
2024-07-17 18:37:22 +0800

    update text component

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -12,16 +12,16 @@ const Text: React.FC<TextSectionProps> = ({
   const paragraphs = Array.isArray(text) ? text : text.split('\n\n');
 
   return (
-    <div className="py-6 sm:py-8 lg:py-10 max-w-2xl text-center mx-auto">
-      <h1 className="text-xl font-semibold tracking-tight text-neutral-900 sm:text-2xl lg:text-3xl xl:text-4xl font-serif">
+    <div className="py-10 lg:py-16 max-w-2xl text-center mx-auto">
+      <h1 className="text-xl font-bold tracking-tight text-neutral-900 sm:text-2xl lg:text-3xl xl:text-4xl">
         {title}
       </h1>
-      <h2 className="text-xs sm:text-sm md:text-md lg:text-lg text-neutral-600 font-inter">
+      <h2 className="text-xs sm:text-sm md:text-md lg:text-lg text-neutral-600 font-light">
         {subtitle}
       </h2>
       <div className="mt-6 sm:mt-8 lg:mt-10">
         {paragraphs.map((paragraph, index) => (
-          <p key={index} className="mt-6 text-neutral-600 text-left text-sm sm:text-md lg:text-base">
+          <p key={index} className="mt-6 text-neutral-600 text-left text-sm sm:text-md lg:text-base font-light">
             {paragraph}
           </p>
         ))}
