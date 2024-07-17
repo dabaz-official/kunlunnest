3f04758c3d0c1b97e624ae955b6b439f8da37ba8
DabAZ <dabaz@dabaz.me>
2024-07-18 01:44:30 +0800

    Update AboutSection.tsx

--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -5,7 +5,7 @@ import Text from '@/components/ui/Text';
 
 export function AboutSection() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-16 sm:py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div>
