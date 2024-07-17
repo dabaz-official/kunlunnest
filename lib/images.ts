477dc97bbe9f7e87bb22054129cfac2bffc4768e
DabAZ <dabaz@dabaz.me>
2024-07-18 01:03:18 +0800

    chore: create images.ts

---
+++ lib/images.ts
@@ -0,0 +1,34 @@
+export const images = {
+  banner: [
+    {
+      src: '/images/home/hero.jpg',
+      alt: '昆仑巢的照片。',
+      width: 1920,
+      height: 800,
+    },
+    {
+      src: '/images/home/hero.jpg',
+      alt: '昆仑巢的照片。',
+      width: 1920,
+      height: 800,
+    },
+    {
+      src: '/images/home/hero.jpg',
+      alt: '昆仑巢的照片。',
+      width: 1920,
+      height: 800,
+    },
+    {
+      src: '/images/home/hero.jpg',
+      alt: '昆仑巢的照片。',
+      width: 1920,
+      height: 800,
+    },
+    {
+      src: '/images/home/hero.jpg',
+      alt: '昆仑巢的照片。',
+      width: 1920,
+      height: 800,
+    },
+  ],
+}
