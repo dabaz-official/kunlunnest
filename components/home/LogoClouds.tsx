b79d9474f06436a2f12b6617692ad6185e2964cb
DabAZ <dabaz@dabaz.me>
2024-07-19 01:16:10 +0800

    update logo clouds

--- components/home/LogoClouds.tsx
+++ components/home/LogoClouds.tsx
@@ -8,7 +8,7 @@ export function LogoClouds() {
         <h1 className="mt-2 text-3xl font-bold text-neutral-900 sm:text-4xl">
           创新生态合作伙伴
         </h1>
-        <div className="mx-auto mt-16 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-16 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           {logos.companies.map((logo) => (
             <div
               key={logo.alt}
@@ -19,7 +19,7 @@ export function LogoClouds() {
                 alt={logo.alt}
                 width={logo.width}
                 height={100}
-                className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+                className='col-span-2 max-h-6 w-full object-contain lg:col-span-1'
               />
               <p className='text-sm text-neutral-500'>
                 {logo.alt}
