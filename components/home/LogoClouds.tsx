2defefb4951c82e4f97e9190fa26b1f54a321e35
DabAZ <dabaz@dabaz.me>
2024-07-19 00:04:27 +0800

    feat: add logos

--- components/home/LogoClouds.tsx
+++ components/home/LogoClouds.tsx
@@ -1,116 +1,31 @@
+import { logos } from '@/lib/logos';
+import Image from 'next/image';
+
 export function LogoClouds() {
   return (
     <div className="bg-white py-16 sm:py-24">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-          由以下企业...
-        </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
-            alt="Transistor"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Reform"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Tuple"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          />
-          <img
-            alt="Statamic"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          />
-          <img
-            alt="Transistor"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Reform"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Tuple"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          />
-          <img
-            alt="Statamic"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          />
-          <img
-            alt="Transistor"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Reform"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Tuple"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          />
-          <img
-            alt="Statamic"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          />
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
+        <h1 className="mt-2 text-3xl font-bold text-neutral-900 sm:text-4xl">
+          创新生态合作伙伴
+        </h1>
+        <div className="mx-auto mt-16 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          {logos.companies.map((logo) => (
+            <div
+              key={logo.alt}
+              className='flex flex-col gap-y-6'
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                width={logo.width}
+                height={100}
+                className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+              />
+              <p className='text-sm text-neutral-500'>
+                {logo.alt}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ lib/logos.ts
@@ -0,0 +1,159 @@
+export const logos = {
+  companies: [
+    {
+      src: '/images/home/logos/baidu.svg',
+      alt: '百度',
+      width: 320,
+    },
+    {
+      src: '/images/home/logos/lenovo.svg',
+      alt: '联想',
+      width: 311,
+    },
+    {
+      src: '/images/home/logos/tencent.svg',
+      alt: '腾讯',
+      width: 744,
+    },
+    {
+      src: '/images/home/logos/xiaomi.svg',
+      alt: '小米',
+      width: 100,
+    },
+    {
+      src: '/images/home/logos/kingsoft.svg',
+      alt: '金山',
+      width: 257,
+    },
+    {
+      src: '/images/home/logos/fengping.svg',
+      alt: '风平智能',
+      width: 462,
+    },
+    {
+      src: '/images/home/logos/xingxing.svg',
+      alt: '行行 AI',
+      width: 100,
+    },
+    {
+      src: '/images/home/logos/aigclink.svg',
+      alt: 'AIGCLINK',
+      width: 113,
+    },
+    {
+      src: '/images/home/logos/itjuzi.svg',
+      alt: 'IT 桔子',
+      width: 113,
+    },
+    {
+      src: '/images/home/logos/qianbidao.svg',
+      alt: '铅笔道',
+      width: 330,
+    },
+    {
+      src: '/images/home/logos/cctv.svg',
+      alt: '中央电视台',
+      width: 276,
+    },
+    {
+      src: '/images/home/logos/cn-news.svg',
+      alt: '中国新闻网',
+      width: 340,
+    },
+    {
+      src: '/images/home/logos/youth.svg',
+      alt: '中国青年网',
+      width: 181,
+    },
+    {
+      src: '/images/home/logos/city-of-design.svg',
+      alt: '北京·设计之都',
+      width: 130,
+    },
+    {
+      src: '/images/home/logos/scrat.svg',
+      alt: '斯克莱特',
+      width: 341,
+    },
+    {
+      src: '/images/home/logos/future-wise.svg',
+      alt: '智造未来',
+      width: 100,
+    },
+    {
+      src: '/images/home/logos/mindful-planet.svg',
+      alt: '一念星球',
+      width: 306,
+    },
+    {
+      src: '/images/home/logos/luobot.svg',
+      alt: '全世萝卜',
+      width: 358,
+    },
+    {
+      src: '/images/home/logos/turing.svg',
+      alt: '图灵教育出版社',
+      width: 164,
+    },
+    {
+      src: '/images/home/logos/synced.svg',
+      alt: '机器之心',
+      width: 500,
+    },
+    {
+      src: '/images/home/logos/intelli-coffee.svg',
+      alt: '智咖时代',
+      width: 407,
+    },
+    {
+      src: '/images/home/logos/cubix-ai.svg',
+      alt: '软积木',
+      width: 88,
+    },
+    {
+      src: '/images/home/logos/tusstar.svg',
+      alt: '启迪之星',
+      width: 210,
+    },
+    {
+      src: '/images/home/logos/ccafa.svg',
+      alt: '中央美术学院',
+      width: 110,
+    },
+    {
+      src: '/images/home/logos/realman.svg',
+      alt: '睿尔曼智能',
+      width: 384,
+    },
+    {
+      src: '/images/home/logos/gxcas.svg',
+      alt: '中科银河芯',
+      width: 351,
+    },
+    {
+      src: '/images/home/logos/channel-wu.svg',
+      alt: '吴晓波频道',
+      width: 170,
+    },
+    {
+      src: '/images/home/logos/xin-jing.svg',
+      alt: '新菁集团',
+      width: 380,
+    },
+    {
+      src: '/images/home/logos/guangqijingming.svg',
+      alt: '光启景明',
+      width: 71,
+    },
+    {
+      src: '/images/home/logos/chengfangdashujv.svg',
+      alt: '乘方大数据',
+      width: 100,
+    },
+    {
+      src: '/images/home/logos/csdn.svg',
+      alt: 'CSDN',
+      width: 340,
+    },
+  ],
+}
