2adb035f86ce460b5ff7d60dd3e619a0716937e8
DabAZ <dabaz@dabaz.me>
2024-08-03 05:07:08 +0800

    update metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
+
 import AboutHero from '@/components/about/AboutHero';
 import AboutTimeline from '@/components/about/AboutTimeline';
+
+export const metadata: Metadata = {
+  title: '关于',
+  description: '昆仑巢，如同一片火花四溅的舞台，将不同领域的智慧和创意汇聚一堂。',  
+}
 
 export default function About() {
   return (
