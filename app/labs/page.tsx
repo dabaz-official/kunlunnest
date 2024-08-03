78ef861425f795e49cbb5c9ef7608fff283737a4
DabAZ <dabaz@dabaz.me>
2024-08-03 21:44:14 +0800

    feat: add labs page

---
+++ app/labs/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+
+import LabsNavigation from '@/components/labs/LabsNavigation';
+
+export const metadata: Metadata = {
+  title: '开放实验室',
+  description: '昆仑巢，如同一片火花四溅的舞台，将不同领域的智慧和创意汇聚一堂。',  
+}
+
+export default function News() {
+  return (
+    <main className="isolate">
+      <LabsNavigation />
+    </main>
+  )
+}
---
+++ components/labs/LabsNavigation.tsx
@@ -0,0 +1,86 @@
+import Title from '@/components/ui/Title';
+
+export default function LabsNavigation() {
+  return (
+    <div className="bg-white relative isolate -z-10 pt-14">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <Title
+          title="开放实验室"
+          description="了解昆仑巢的开放实验室。"
+        />
+
+        <div className="mt-16 grid grid-cols-1 gap-y-6 sm:mt-20 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+          <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
+            <img
+              src="/images/labs/3d.webp"
+              alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
+              className="object-cover object-center group-hover:opacity-75 transition-all"
+            />
+            <div aria-hidden="true" className="bg-gradient-to-b from-transparent to-black opacity-50" />
+            <div className="flex items-end p-6">
+              <div className="group-hover:ml-2 transition-all">
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    3D 打印开放实验室
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  深入了解 -&gt;
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
+            <img
+              src="/images/labs/aigc.webp"
+              alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters."
+              className="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full transition-all"
+            />
+            <div
+              aria-hidden="true"
+              className="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"
+            />
+            <div className="flex items-end p-6 sm:absolute sm:inset-0">
+              <div className="group-hover:ml-2 transition-all">
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    AIGC 开放实验室
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  深入了解 -&gt;
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full">
+            <img
+              src="/images/labs/robot.webp"
+              alt="Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk."
+              className="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full transition-all"
+            />
+            <div
+              aria-hidden="true"
+              className="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"
+            />
+            <div className="flex items-end p-6 sm:absolute sm:inset-0">
+              <div className="group-hover:ml-2 transition-all">
+                <h3 className="font-semibold text-white">
+                  <a href="#">
+                    <span className="absolute inset-0" />
+                    人形机器人开放实验室
+                  </a>
+                </h3>
+                <p aria-hidden="true" className="mt-1 text-sm text-white">
+                  深入了解 -&gt;
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
